824c5accca39d789baed0963c4f968eddeb82b64
Anand Thakker <vestibule@anandthakker.net>
2016-06-10 22:24:23 -0400

    Load more button

--- segnet/dist/view.js
+++ segnet/dist/view.js
@@ -3247,12 +3247,13 @@ const app = choo()
 
 app.model({
   namespace: 'app',
-  state: { items: [] },
+  state: { items: [], limit: 50 },
   subscriptions: [
     function (send) { send('http:get_json') } // grab json data at startup
   ],
   reducers: {
-    'setTestOutput': (action, state) => ({ items: action.payload })
+    'setTestOutput': (action, state) => ({ items: action.payload }),
+    'loadMore': logged((action, state) => ({ limit: state.limit + 50 }), 'loadMore')
   },
   effects: {
     'error': (state, event) => console.error(`error: ${event.payload}`),
@@ -3270,6 +3271,7 @@ app.model({
 const view = (params, state, send) => choo.view`
   <main>
   ${state.app.items
+    .slice(0, state.app.limit)
     .map(item => choo.view`
          <div>
            <img src=${item.input}></img>
@@ -3278,11 +3280,14 @@ const view = (params, state, send) => choo.view`
          </div>
     `)
   }
+  ${state.app.limit < state.app.items.length
+    ? choo.view`<button onclick=${() => send('app:loadMore')}>Load More</button>`
+    : ''}
   </main>
 `
 
 app.router((route) => [
-  route('/', view)
+  route('/', logged(view, 'view'))
 ])
 
 document.body.appendChild(app.start())
@@ -3296,6 +3301,13 @@ function getJson (state, action, send) {
     }
     send('app:setTestOutput', { payload: body })
   })
+}
+
+function logged (view, tag) {
+  return function () {
+    console.log(tag || '', arguments)
+    return view.apply(this, Array.prototype.slice.call(arguments))
+  }
 }
 
 },{"choo":6,"choo/http":5}]},{},[35]);
--- segnet/view.js
+++ segnet/view.js
@@ -5,12 +5,13 @@ const app = choo()
 
 app.model({
   namespace: 'app',
-  state: { items: [] },
+  state: { items: [], limit: 50 },
   subscriptions: [
     function (send) { send('http:get_json') } // grab json data at startup
   ],
   reducers: {
-    'setTestOutput': (action, state) => ({ items: action.payload })
+    'setTestOutput': (action, state) => ({ items: action.payload }),
+    'loadMore': logged((action, state) => ({ limit: state.limit + 50 }), 'loadMore')
   },
   effects: {
     'error': (state, event) => console.error(`error: ${event.payload}`),
@@ -28,6 +29,7 @@ app.model({
 const view = (params, state, send) => choo.view`
   <main>
   ${state.app.items
+    .slice(0, state.app.limit)
     .map(item => choo.view`
          <div>
            <img src=${item.input}></img>
@@ -36,11 +38,14 @@ const view = (params, state, send) => choo.view`
          </div>
     `)
   }
+  ${state.app.limit < state.app.items.length
+    ? choo.view`<button onclick=${() => send('app:loadMore')}>Load More</button>`
+    : ''}
   </main>
 `
 
 app.router((route) => [
-  route('/', view)
+  route('/', logged(view, 'view'))
 ])
 
 document.body.appendChild(app.start())
@@ -54,4 +59,11 @@ function getJson (state, action, send) {
     }
     send('app:setTestOutput', { payload: body })
   })
+}
+
+function logged (view, tag) {
+  return function () {
+    console.log(tag || '', arguments)
+    return view.apply(this, Array.prototype.slice.call(arguments))
+  }
 }
