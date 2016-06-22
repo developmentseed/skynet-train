783180bf4647196e2b7e664d42eaf1a9231f13f5
Anand Thakker <vestibule@anandthakker.net>
2016-06-21 20:59:03 -0400

    tabs => spaces

--- segnet/test_segmentation.py
+++ segnet/test_segmentation.py
@@ -27,9 +27,9 @@ parser.add_argument('--classes', type=str, required=True)
 args = parser.parse_args()
 
 with open(args.classes) as classes:
-	colors = map(lambda x : x['color'][1:], json.load(classes))
-	colors.append('000000')
-	colors = map(lambda rgbstr : tuple(map(ord, rgbstr.decode('hex'))), colors)
+    colors = map(lambda x : x['color'][1:], json.load(classes))
+    colors.append('000000')
+    colors = map(lambda rgbstr : tuple(map(ord, rgbstr.decode('hex'))), colors)
 
 model = open(args.model, 'r').read()
 m = re.search('source:\s*"(.*)"', model)
@@ -47,60 +47,60 @@ outputs = []
 
 for i in range(0, len(test_data)):
 
-	net.forward()
+    net.forward()
 
-	image = net.blobs['data'].data
-	label = net.blobs['label'].data
-	predicted = net.blobs['prob'].data
-	image = np.squeeze(image[0,:,:,:])
-	output = np.squeeze(predicted[0,:,:,:])
-	ind = np.argmax(output, axis=0)
+    image = net.blobs['data'].data
+    label = net.blobs['label'].data
+    predicted = net.blobs['prob'].data
+    image = np.squeeze(image[0,:,:,:])
+    output = np.squeeze(predicted[0,:,:,:])
+    ind = np.argmax(output, axis=0)
 
-	r = ind.copy()
-	g = ind.copy()
-	b = ind.copy()
-	r_gt = label.copy()
-	g_gt = label.copy()
-	b_gt = label.copy()
+    r = ind.copy()
+    g = ind.copy()
+    b = ind.copy()
+    r_gt = label.copy()
+    g_gt = label.copy()
+    b_gt = label.copy()
 
-	label_colours = np.array(colors)
-	for l in range(0,len(colors)):
-		r[ind==l] = label_colours[l,0]
-		g[ind==l] = label_colours[l,1]
-		b[ind==l] = label_colours[l,2]
-		r_gt[label==l] = label_colours[l,0]
-		g_gt[label==l] = label_colours[l,1]
-		b_gt[label==l] = label_colours[l,2]
+    label_colours = np.array(colors)
+    for l in range(0,len(colors)):
+        r[ind==l] = label_colours[l,0]
+        g[ind==l] = label_colours[l,1]
+        b[ind==l] = label_colours[l,2]
+        r_gt[label==l] = label_colours[l,0]
+        g_gt[label==l] = label_colours[l,1]
+        b_gt[label==l] = label_colours[l,2]
 
-	rgb = np.zeros((ind.shape[0], ind.shape[1], 3))
-	rgb[:,:,0] = r
-	rgb[:,:,1] = g
-	rgb[:,:,2] = b
-	rgb_gt = np.zeros((ind.shape[0], ind.shape[1], 3))
-	rgb_gt[:,:,0] = r_gt
-	rgb_gt[:,:,1] = g_gt
-	rgb_gt[:,:,2] = b_gt
+    rgb = np.zeros((ind.shape[0], ind.shape[1], 3))
+    rgb[:,:,0] = r
+    rgb[:,:,1] = g
+    rgb[:,:,2] = b
+    rgb_gt = np.zeros((ind.shape[0], ind.shape[1], 3))
+    rgb_gt[:,:,0] = r_gt
+    rgb_gt[:,:,1] = g_gt
+    rgb_gt[:,:,2] = b_gt
 
-	image = np.transpose(image, (1,2,0))
-	output = np.transpose(output, (1,2,0))
-	image = image[:,:,(2,1,0)]
+    image = np.transpose(image, (1,2,0))
+    output = np.transpose(output, (1,2,0))
+    image = image[:,:,(2,1,0)]
 
-	prediction = str(i) + '_prediction.png'
-	input_image = str(i) + '_input.png'
-	groundtruth = str(i) + '_groundtruth.png'
-	scipy.misc.toimage(rgb, cmin=0.0, cmax=255).save(os.path.join(args.output, prediction))
-	scipy.misc.toimage(image, cmin=0.0, cmax=255).save(os.path.join(args.output, input_image))
-	scipy.misc.toimage(rgb_gt, cmin=0.0, cmax=255).save(os.path.join(args.output, groundtruth))
-	outputs.append({
-		'index': i,
-		'input': input_image,
-		'prediction': prediction,
-		'groundtruth': groundtruth,
-		'test_data': test_data[i]
-	})
+    prediction = str(i) + '_prediction.png'
+    input_image = str(i) + '_input.png'
+    groundtruth = str(i) + '_groundtruth.png'
+    scipy.misc.toimage(rgb, cmin=0.0, cmax=255).save(os.path.join(args.output, prediction))
+    scipy.misc.toimage(image, cmin=0.0, cmax=255).save(os.path.join(args.output, input_image))
+    scipy.misc.toimage(rgb_gt, cmin=0.0, cmax=255).save(os.path.join(args.output, groundtruth))
+    outputs.append({
+        'index': i,
+        'input': input_image,
+        'prediction': prediction,
+        'groundtruth': groundtruth,
+        'test_data': test_data[i]
+    })
 
 with open(os.path.join(args.output, 'index.json'), 'w') as outfile:
-	json.dump(outputs, outfile)
+    json.dump(outputs, outfile)
 
 print 'Success!'
 
