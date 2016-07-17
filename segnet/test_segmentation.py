80fa79bccec321a000487a836efc6ebf85c8ce74
Anand Thakker <vestibule@anandthakker.net>
2016-07-16 22:40:59 -0400

    Add transparency to test output

--- segnet/test_segmentation.py
+++ segnet/test_segmentation.py
@@ -14,6 +14,7 @@ sys.path.insert(0, caffe_root + 'python')
 
 import caffe
 
+
 # Import arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, required=True)
@@ -27,6 +28,8 @@ with open(args.classes) as classes:
     colors.append('000000')
     colors = map(lambda rgbstr: tuple(map(ord, rgbstr.decode('hex'))), colors)
 
+num_classes = len(colors)
+
 model = open(args.model, 'r').read()
 m = re.search('source:\s*"(.*)"', model)
 test_data = m.group(1)
@@ -37,7 +40,6 @@ caffe.set_mode_gpu()
 net = caffe.Net(args.model,
                 args.weights,
                 caffe.TEST)
-
 
 outputs = []
 
@@ -50,14 +52,24 @@ for i in range(0, len(test_data)):
     predicted = net.blobs['prob'].data
     image = np.squeeze(image[0, :, :, :])
     output = np.squeeze(predicted[0, :, :, :])
+
+    fg = output[:-1, :, :]
+
     ind = np.argmax(output, axis=0)
+    bg = ind.copy()
+    bg[:] = num_classes - 1
+    # only use the max-probability non-background class if its probability is
+    # above some threshold
+    ind = np.where(np.max(fg, axis=0) > 0.5, ind, bg)
 
     r = ind.copy()
     g = ind.copy()
     b = ind.copy()
+    a = np.zeros(ind.shape)
     r_gt = label.copy()
     g_gt = label.copy()
     b_gt = label.copy()
+    a_gt = np.zeros(label.shape)
 
     label_colours = np.array(colors)
     for l in range(0, len(colors)):
@@ -68,14 +80,21 @@ for i in range(0, len(test_data)):
         g_gt[label == l] = label_colours[l, 1]
         b_gt[label == l] = label_colours[l, 2]
 
-    rgb = np.zeros((ind.shape[0], ind.shape[1], 3))
+    max_prob = np.max(output, axis=0)
+    a[ind != num_classes - 1] = max_prob[ind != num_classes - 1] * 255
+    a_gt[label != num_classes - 1] = 255
+
+    rgb = np.zeros((ind.shape[0], ind.shape[1], 4))
     rgb[:, :, 0] = r
     rgb[:, :, 1] = g
     rgb[:, :, 2] = b
-    rgb_gt = np.zeros((ind.shape[0], ind.shape[1], 3))
+    rgb[:, :, 3] = a
+
+    rgb_gt = np.zeros((ind.shape[0], ind.shape[1], 4))
     rgb_gt[:, :, 0] = r_gt
     rgb_gt[:, :, 1] = g_gt
     rgb_gt[:, :, 2] = b_gt
+    rgb_gt[:, :, 3] = a_gt
 
     image = np.transpose(image, (1, 2, 0))
     output = np.transpose(output, (1, 2, 0))
@@ -84,11 +103,11 @@ for i in range(0, len(test_data)):
     prediction = str(i) + '_prediction.png'
     input_image = str(i) + '_input.png'
     groundtruth = str(i) + '_groundtruth.png'
-    scipy.misc.toimage(rgb, cmin=0.0, cmax=255).save(
+    scipy.misc.toimage(rgb, cmin=0.0, cmax=255, mode='RGBA').save(
         os.path.join(args.output, prediction))
     scipy.misc.toimage(image, cmin=0.0, cmax=255).save(
         os.path.join(args.output, input_image))
-    scipy.misc.toimage(rgb_gt, cmin=0.0, cmax=255).save(
+    scipy.misc.toimage(rgb_gt, cmin=0.0, cmax=255, mode='RGBA').save(
         os.path.join(args.output, groundtruth))
 
     outputs.append({
@@ -98,7 +117,6 @@ for i in range(0, len(test_data)):
         'groundtruth': groundtruth,
         'test_data': test_data[i]
     })
-    print(outputs[-1])
 
 with open(os.path.join(args.output, 'index.json'), 'w') as outfile:
     json.dump(outputs, outfile)
