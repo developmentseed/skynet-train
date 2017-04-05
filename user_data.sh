01f2471071acf982393f343da3e3e6f2cdcd600d
Drew Bollinger <drew@developmentseed.org>
2017-04-05 15:54:32 -0400

    Update nvidia docker version

--- user_data.sh
+++ user_data.sh
@@ -20,7 +20,7 @@ service docker start
 # https://github.com/NVIDIA/nvidia-docker/wiki/Deploy-on-Amazon-EC2
 apt-get install --no-install-recommends -y gcc make libc-dev
 wget -P /tmp http://us.download.nvidia.com/XFree86/Linux-x86_64/361.42/NVIDIA-Linux-x86_64-361.42.run
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0/nvidia-docker_1.0.0-1_amd64.deb
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
 sh /tmp/NVIDIA-Linux-x86_64-361.42.run --silent
 dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 
